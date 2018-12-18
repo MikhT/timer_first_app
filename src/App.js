5d660b016c46b0b81e0b5595afebd763584fe1b3
Mikhael <tuttoandroid@gmail.com>
2018-12-18 11:26:06 +0100

    Date and time - first react app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+const happy = <h2>I am happy</h2>;
+
+  function getTime(){
+    return new Date().toLocaleTimeString();
+  }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This timer is my first react app
-          </p>
-        </header>
+       <h1>My first react app is a timer</h1>
+          {happy}
+
+          <h2>Oggi è { new Date().toLocaleDateString() + " " + getTime()}</h2>
       </div>
     );
   }
