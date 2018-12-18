51fe7aa7c62f0fa7911d3de2668697d4fe5fa4ea
Mikhael <tuttoandroid@gmail.com>
2018-12-18 12:42:37 +0100

    Timer funzionante come component + DidMount interval e UnMount

--- src/Timer.js
+++ src/Timer.js
@@ -15,9 +15,20 @@ import React from 'react'
             const tempo = this.state.date.getTime() + this.props.timezone*3600*1000;
             const data = new Date(tempo);
 
-            return <h2>Oggi è {data.toLocaleTimeString()}</h2>
+            return <h2>Sono le {data.toLocaleTimeString()}</h2>
         }
 
+        reset = () => {
+            this.setState({date: new Date()});
+        };
+
+        componentDidMount(){
+            this.interval = setInterval( this.reset, 1000);
+        }
+
+        componentWillUnmount() {
+            clearInterval(this.interval);
+        }
 
     }
 
