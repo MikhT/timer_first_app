9c9587bdc9021226382bfc1ddea272bc1c4ab019
Mikhael <tuttoandroid@gmail.com>
2018-12-18 11:39:03 +0100

    Timer as component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer'
 
 const happy = <h2>I am happy</h2>;
 
-  function getTime(){
-    return new Date().toLocaleTimeString();
-  }
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
        <h1>My first react app is a timer</h1>
+
           {happy}
 
-          <h2>Oggi è { new Date().toLocaleDateString() + " " + getTime()}</h2>
+          <Timer></Timer>
+          <Timer></Timer>
+          <Timer></Timer>
+
       </div>
     );
   }
